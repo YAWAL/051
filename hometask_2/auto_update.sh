1f99d736b9c48d1a03883867c656d974c8ee19d0
YAWAL <yavorskyyval@gmail.com>
2020-02-02 16:13:53 +0100

    added auto updating script

--- hometask_2/auto_update.sh
+++ hometask_2/auto_update.sh
@@ -4,4 +4,8 @@
 ## The updated log file should be stored in /var/log/update-YYYYMMDD.log, where YYYY-year, MM-month, DD-day_of_month.
 ##***Extra task: configure log rotation to update-YYYYMMDD.log every month and keep the last 3 log files compressed.
 
-
+(sudo apt-get update && sudo apt-get -y upgrade) | sudo tee /var/log/update-"$(date +"%Y%m%d")".log 
+crontab -l > mycron
+echo "* * * * 7 $pwd/auto_update.sh" >> mycron
+crontab mycron
+rm mycron
